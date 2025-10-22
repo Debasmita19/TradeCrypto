347b266599854fc700601a9b67533a92a486a5b5
Debasmita <debasmita.biswas2019@gmail.com>
2025-10-22 01:27:38 -0400

    Initial commit

---
+++ TradeCrypto/src/main/java/tradeCrypto/TradeCryptoApplication.java
@@ -0,0 +1,13 @@
+package tradeCrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeCryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeCryptoApplication.class, args);
+	}
+
+}
---
+++ TradeCrypto/src/main/java/tradeCrypto/modal/User.java
@@ -0,0 +1,26 @@
+package tradeCrypto.modal;
+package packageName;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+
+}
